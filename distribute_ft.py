e9f9010161ecdf542a4743712178f630aea94b7d
xxJTxx <135737491+xxJTxx@users.noreply.github.com>
2024-02-21 01:04:44 +0800

    Update distribute_ft.py
    hook the function of record every input of conv into corresponding list

--- distribute_ft.py
+++ distribute_ft.py
@@ -1,8 +1,16 @@
 
 import torch
+import torch.nn as nn
 from models import mnist, cifar10, resnet, queries
 from torchsummary import summary
 from loaders import get_cifar10_loaders, get_cifar100_loaders, get_svhn_loaders, get_mnist_loaders
+
+# Custom Loss function that take into two models water_model and train_model, and return the mean squared error between output of certain layers
+class custom_loss(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, water_relu, train_relu):
+        return torch.mean(torch.pow((water_relu - train_relu), 2))
 
 CIFAR_QUERY_SIZE = (3, 32, 32)
 response_scale = 10
@@ -38,9 +46,47 @@ original_response = checkpoint['query_model']['state_dict']['original_response']
 water_relu = []
 train_relu = []
 
-# Hook the function onto conv1 and conv2 of layer1~layer4
-for name, module in water_model[1].layer1[0].named_children():
-    if name in ['conv1','conv2']:
-        print(getattr(water_model[1].layer1[0], name))
-        print(name)
-        print(module)
+# Define the hook function
+w_hooks = [] # list of hook handles, to be removed when you are done
+t_hooks = []
+def water_hook(module, input, output):
+    water_relu.append(input)
+    
+def train_hook(module, input, output):
+    train_relu.append(input)
+
+
+# Hook the function onto conv1 and conv2 of layer1~layer4 of both models
+for idx in range(len(water_model[1].layer1)):
+    for name, module in water_model[1].layer1[idx].named_children():
+        if name in ['conv1','conv2']:
+            w_hooks.append(getattr(water_model[1].layer1[idx], name).register_forward_hook(water_hook))
+for idx in range(len(water_model[1].layer2)):
+    for name, module in water_model[1].layer2[idx].named_children():
+        if name in ['conv1','conv2']:
+            w_hooks.append(getattr(water_model[1].layer2[idx], name).register_forward_hook(water_hook))
+for idx in range(len(water_model[1].layer3)):
+    for name, module in water_model[1].layer3[idx].named_children():
+        if name in ['conv1','conv2']:
+            w_hooks.append(getattr(water_model[1].layer3[idx], name).register_forward_hook(water_hook))
+for idx in range(len(water_model[1].layer4)):
+    for name, module in water_model[1].layer4[idx].named_children():
+        if name in ['conv1','conv2']:
+            w_hooks.append(getattr(water_model[1].layer4[idx], name).register_forward_hook(water_hook))
+
+for idx in range(len(train_model[1].layer1)):
+    for name, module in train_model[1].layer1[idx].named_children():
+        if name in ['conv1','conv2']:
+            t_hooks.append(getattr(train_model[1].layer1[idx], name).register_forward_hook(train_hook))
+for idx in range(len(train_model[1].layer2)):
+    for name, module in train_model[1].layer2[idx].named_children():
+        if name in ['conv1','conv2']:
+            t_hooks.append(getattr(train_model[1].layer2[idx], name).register_forward_hook(train_hook))
+for idx in range(len(train_model[1].layer3)):
+    for name, module in train_model[1].layer3[idx].named_children():
+        if name in ['conv1','conv2']:
+            t_hooks.append(getattr(train_model[1].layer3[idx], name).register_forward_hook(train_hook))
+for idx in range(len(train_model[1].layer4)):
+    for name, module in train_model[1].layer4[idx].named_children():
+        if name in ['conv1','conv2']:
+            t_hooks.append(getattr(train_model[1].layer4[idx], name).register_forward_hook(train_hook))
