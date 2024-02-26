2fb843bb918170733ed03413a235eba6208b8e81
xxJTxx <135737491+xxJTxx@users.noreply.github.com>
2024-02-26 22:50:29 +0800

    Update distribute_ft.py
    training function finished
    neuron_loss seems weird

--- distribute_ft.py
+++ distribute_ft.py
@@ -6,12 +6,16 @@ from models import mnist, cifar10, resnet, queries
 from torchsummary import summary
 from loaders import get_cifar10_loaders, get_cifar100_loaders, get_svhn_loaders, get_mnist_loaders,get_cifar10_loaders_sub
 
+import pdb
+
 # Custom Loss function that take into two models water_model and train_model, and return the mean squared error between output of certain layers
 class custom_loss(nn.Module):
     def __init__(self):
         super().__init__()
     def forward(self, water_relu, train_relu):
         return torch.mean(torch.pow((water_relu - train_relu), 2))
+        
+
 
 # Define the training loop
 def start_train(train_model, water_model, train_loader, val_loader, optimizer, device, num_epochs=10, new_loss_r=0):
@@ -30,11 +34,17 @@ def start_train(train_model, water_model, train_loader, val_loader, optimizer, d
             # Define the hook function
             w_hooks = [] # list of hook handles, to be removed when you are done
             t_hooks = []
+            hook_flag = True
             def water_hook(module, input, output):
+                if not hook_flag:
+                    return
+                nonlocal water_relu
                 water_relu.append(input)   
             def train_hook(module, input, output):
+                if not hook_flag:
+                    return
+                nonlocal train_relu
                 train_relu.append(input)
-
 
             # Hook the function onto conv1 and conv2 of layer1~layer4 of both models.
             for idx in range(len(water_model[1].layer1)):
@@ -69,24 +79,16 @@ def start_train(train_model, water_model, train_loader, val_loader, optimizer, d
             for idx in range(len(train_model[1].layer4)):
                 for name, module in train_model[1].layer4[idx].named_children():
                     if name in ['conv1','conv2']:
-                        t_hooks.append(getattr(train_model[1].layer4[idx], name).register_forward_hook(train_hook))
-            """ 
+                        t_hooks.append(getattr(train_model[1].layer4[idx], name).register_forward_hook(train_hook)) """
         
         print(f"Now in epoch {epoch+1}...")
         
-        train_model.train()
-        water_model.train() # Not sure if it's ok to use .eval() and abandon with .no_grad() below?
-        print("Models in training mode...")
-        
-        running_loss = 0.0
-        neuron_loss = 0.0
-        conbine_loss = 0.0
-        new_loss = 0.0
-        print("Loss initialized...")
         new_loss_func = custom_loss()
-        print("New loss function created...")
 
         for batch_idx, batch in enumerate(train_loader):
+            train_model.train()
+            water_model.train() # Not sure if it's ok to use .eval() and abandon with .no_grad() below?
+            
             print(f"Process {batch_idx+1} batch...")
             optimizer.zero_grad()
             images = batch[0]
@@ -94,47 +96,51 @@ def start_train(train_model, water_model, train_loader, val_loader, optimizer, d
 
             # Record the input of every hooked layer
             water_relu = []
-            train_relu = [] 
-
+            train_relu = []
+            
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = train_model(images)
             with torch.no_grad():
-                water_model(images)
-            ce_loss = F.cross_entropy(outputs, labels)
-            print(ce_loss.item())
+                water_model(images) 
+            ce_loss = (1-new_loss_r)*F.cross_entropy(outputs, labels)
+            print("Current batch ce loss: ", ce_loss.item())
+            print("Current batch ce loss grad: ", ce_loss.grad_fn)
                 
             if not water_relu and not train_relu:
                 print("No value stored in fix_relu and train_relu...")
             else:
-                print(f"{len(water_relu)} layers are store in the lists...")    
+                print(f"{len(water_relu)} layers are store in the water_lists...")
+                print(f"{len(train_relu)} layers are store in the train_lists...")    
             
+            # Reset new_loss
+            new_loss = 0.0
             # Sum up the loss of conv1 and conv2 of layer1~layer4 of both models
             for idx in range(len(water_relu)):
-                new_loss += new_loss_func(water_relu[idx][0], train_relu[idx][0])
+                new_loss += (new_loss_r)*new_loss_func(water_relu[idx][0], train_relu[idx][0])
             # Just for debug
-            if batch_idx == 0:
-                    print("first batch neuron loss: ", new_loss.item())    
+            print("current batch neuron loss: ", new_loss.item())
+            print("Current batch neuron loos: ", new_loss.grad_fn)    
             
             # Combine both losses
-            ((1-new_loss_r)*ce_loss - (new_loss_r)*new_loss).backward()
+            loss = ce_loss - new_loss
+            print("Current batch combined loss: ", loss.item())
+            print("Current batch combined loos: ", loss.grad_fn) 
+            #pdb.set_trace()
+            loss.backward()
             optimizer.step()
             
-            # Reset the lists
-            water_relu = []
-            train_relu = []
+            #del water_relu, train_relu
             
-            neuron_loss += new_loss.item() * images.size(0)
-            running_loss += ce_loss.item() * images.size(0)
+            #neuron_loss += new_loss.item() * images.size(0)
+            #running_loss += ce_loss.mean().item() * images.size(0)
             #combine_loss += ((1-new_loss_r)*ce_loss - (new_loss_r)*new_loss) * inputs.size(0)
         
-        epoch_loss = running_loss / len(train_loader.dataset)
-        epoch_n_loss = neuron_loss / len(train_loader.dataset)
-        print(f"Epoch {epoch+1}/{num_epochs}, Training Loss: {epoch_loss:.4f}")
-        print(f"Epoch {epoch+1}/{num_epochs}, Neuron Loss: {epoch_n_loss:.4f}")
+        print("===============================1 epoch of training ends===============================")
 
         # Validate the model
         train_model.eval()
+        hook_flag = False # Turn off the hook for validation
         val_loss = 0.0
         correct = 0
         total = 0
@@ -164,104 +170,13 @@ def start_train(train_model, water_model, train_loader, val_loader, optimizer, d
     
     print('Finished Training') 
     
-""" def start_train(train_model, fix_model, train_loader, val_loader, optimizer, device, water_relu, train_relu, num_epochs=10, new_loss_r=0):
-    
-    # To save the best model on val set
-    #best_val_nat_acc = 0
-    #best_val_query_acc = 0
-    
-    for epoch in range(num_epochs):
-        
-        print(f"Now in epoch {epoch+1}...")
-        
-        train_model.train()
-        fix_model.train() # Not sure if it's ok to use .eval() and abandon with .no_grad() below?
-        print("Models in training mode...")
-        
-        running_loss = 0.0
-        neuron_loss = 0.0
-        conbine_loss = 0.0
-        new_loss = 0.0
-        print("Loss initialized...")
-        new_loss_func = custom_loss()
-        print("New loss function created...")
-
-        for batch_idx, batch in enumerate(train_loader):
-            print(f"Process {batch_idx+1} batch...")
-            optimizer.zero_grad()
-            images = batch[0]
-            labels = batch[1].long()
-
-            # Record the input of every hooked layer
-            water_relu = []
-            train_relu = [] 
-
-            images, labels = images.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = train_model(images)
-            with torch.no_grad():
-                fix_model(images)
-            ce_loss = F.cross_entropy(outputs, labels)
-            print(ce_loss.item())
-                
-            if not water_relu and not train_relu:
-                print("No value stored in fix_relu and train_relu...")
-            else:
-                print(f"{len(water_relu)} layers are store in the lists...")    
-            
-            # Sum up the loss of conv1 and conv2 of layer1~layer4 of both models
-            for idx in range(len(water_relu)):
-                new_loss += new_loss_func(water_relu[idx][0], train_relu[idx][0])
-            # Just for debug
-            if batch_idx == 0:
-                    print("first batch neuron loss: ", new_loss.item())    
-            
-            # Combine both losses
-            ((1-new_loss_r)*ce_loss - (new_loss_r)*new_loss).backward()
-            optimizer.step()
-            
-            # Reset the lists
-            water_relu = []
-            train_relu = []
-            
-            neuron_loss += new_loss.item() * images.size(0)
-            running_loss += ce_loss.item() * images.size(0)
-            #combine_loss += ((1-new_loss_r)*ce_loss - (new_loss_r)*new_loss) * inputs.size(0)
-        
-        epoch_loss = running_loss / len(train_loader.dataset)
-        epoch_n_loss = neuron_loss / len(train_loader.dataset)
-        print(f"Epoch {epoch+1}/{num_epochs}, Training Loss: {epoch_loss:.4f}")
-        print(f"Epoch {epoch+1}/{num_epochs}, Neuron Loss: {epoch_n_loss:.4f}")
-
-        # Validate the model
-        train_model.eval()
-        val_loss = 0.0
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for batch_idx, batch in enumerate(val_loader):
-                print("Validation Process...")
-                images = batch[0]
-                labels = batch[1].long()
-                images, labels = images.to(device), labels.to(device)
-                outputs = train_model(images)
-                loss = F.cross_entropy(outputs, labels)
-                val_loss += loss.item() * images.size(0)
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-            val_epoch_loss = val_loss / len(val_loader.dataset)
-            val_accuracy = correct / total
-            print(f"Epoch {epoch+1}/{num_epochs}, Validation Loss: {val_epoch_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}")
-
-    print('Finished Training')  """
 
 if __name__ == "__main__":
     # Hyperparameters setting 
     dataset = 'cifar10'
     subset_rate = 0.1 # 0~1
     epoch = 1
-    new_loss_ratio = 0.1 # 0~1
+    new_loss_ratio = 0.5 # 0~1
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load the existing checkpoint
@@ -293,7 +208,7 @@ if __name__ == "__main__":
     train_model.to(device)
     water_model.to(device)
 
-    # Record the input of every hooked layer
+    """ # Record the input of every hooked layer
     water_relu = []
     train_relu = []
     
@@ -303,10 +218,10 @@ if __name__ == "__main__":
     def water_hook(module, input, output):
         water_relu.append(input)   
     def train_hook(module, input, output):
-        train_relu.append(input)
+        train_relu.append(input) """
 
 
-    # Hook the function onto conv1 and conv2 of layer1~layer4 of both models.
+    """ # Hook the function onto conv1 and conv2 of layer1~layer4 of both models.
     for idx in range(len(water_model[1].layer1)):
         for name, module in water_model[1].layer1[idx].named_children():
             if name in ['conv1','conv2']:
@@ -339,7 +254,7 @@ if __name__ == "__main__":
     for idx in range(len(train_model[1].layer4)):
         for name, module in train_model[1].layer4[idx].named_children():
             if name in ['conv1','conv2']:
-                t_hooks.append(getattr(train_model[1].layer4[idx], name).register_forward_hook(train_hook))
+                t_hooks.append(getattr(train_model[1].layer4[idx], name).register_forward_hook(train_hook)) """
  
 
     """ # Create testing sample
@@ -349,7 +264,7 @@ if __name__ == "__main__":
 
     # Generate subset data loader based on dataset
     if dataset == 'cifar10':
-        train_loader, valid_loader, test_loader = get_cifar10_loaders() #_sub(subset_rate)
+        train_loader, valid_loader, test_loader = get_cifar10_loaders_sub(subset_rate)
     elif dataset == 'cifar100':
         train_loader, valid_loader, test_loader = get_cifar100_loaders()
     elif dataset == 'svhn':
