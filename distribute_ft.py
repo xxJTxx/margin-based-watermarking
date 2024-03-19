4fcc1a253e01dd8f91253aebbc95a058a8508084
xxJTxx <135737491+xxJTxx@users.noreply.github.com>
2024-03-19 19:02:46 +0800

    Update distribute_ft.py
    1. Neuron loss 2.0 implement

--- distribute_ft.py
+++ distribute_ft.py
@@ -17,6 +17,8 @@ class custom_loss(nn.Module):
         t_denominator=train_relu.detach()       
         w_n = torch.where(water_relu !=0, water_relu/w_denominator, water_relu)
         t_n = torch.where(train_relu !=0, train_relu/t_denominator, train_relu)
+        print(">>>>>>>>>>>>>>>check water norm:",w_n)
+        print(">>>>>>>>>>>>>>>check train norm:",t_n)
         return torch.mean(torch.pow((w_n - t_n), 2))
     
 def loss_fn_kd(outputs, labels, teacher_outputs, alpha, temperature):
@@ -36,6 +38,33 @@ def loss_fn_kd(outputs, labels, teacher_outputs, alpha, temperature):
               F.cross_entropy(outputs, labels).cuda(device) * (1. - alpha)
 
     return KD_loss
+
+def custom_loss1(water_model, train_model, trigger=1e-5):
+        # Denominator take abs value to maintain the sign
+        w_denominator=torch.abs(water_model.detach())
+        t_denominator=torch.abs(train_model.detach()) 
+        
+        # Creating a new tensor where negative/positive values are changed to -1/1, wheras 0 becomes 0+trigger
+        water_one = torch.FloatTensor(water_model.size()).type_as(water_model)
+        mask_w = water_model!=0
+        water_one[mask_w] = water_model[mask_w]/w_denominator[mask_w]
+        mask_w = water_model==0
+        water_one[mask_w] = water_model[mask_w] + trigger
+
+        # Creating a new tensor where negative/positive values are changed to -1/1, wheras 0 remains 0
+        mask = train_model==0
+        train_one = torch.FloatTensor(train_model.size()).type_as(train_model)
+        train_one[mask] = train_model[mask]
+        mask = train_model!=0
+        train_one[mask] = train_model[mask]/t_denominator[mask]
+        
+        #
+        water_one[mask_w] = torch.sign(train_one[mask_w]-1)*water_one[mask_w]
+
+       
+        ''' for idx in range(len(train_one)):
+          print(f"w_o:{water_one[idx]}~~~t_o:{train_one[idx].item(),}") '''
+        return torch.mean(torch.pow((1 + water_one*train_one), 2))
 
 # Define the training loop
 def start_train(dataset, subset_rate, train_model, water_model, optimizer, device, query, response,num_epochs=10, new_loss_r=0, default_loss_r=1, excluded_index=None):
@@ -653,6 +682,8 @@ def start_train_kd_r(dataset, subset_rate, train_model, water_model, optimizer, 
             # Sum up the loss of conv1 and conv2 of layer1~layer4 of both models
             for idx in range(len(water_relu)):
                 new_loss += new_loss_func(water_relu[idx][0].detach(), train_relu[idx][0]) / len(water_relu)
+                print(">>>>>>>>>>>>>>>>>>>>>new loss:",new_loss.item())
+                breakpoint()
             # Just for debug
             if batch_idx % 5 == 0:
                 print(f"{batch_idx+1} batch neuron loss: {new_loss.item()}")    
@@ -738,6 +769,210 @@ def start_train_kd_r(dataset, subset_rate, train_model, water_model, optimizer, 
     print(f"K.D. loss after every epoch: {crosse_loss_after_epoch}")    
     return train_test_acc, train_query_acc, water_test_acc, water_query_acc       
 
+def start_train_kd_loss1(dataset, subset_rate, train_model, water_model, optimizer, device, query, response,num_epochs=10, new_loss_r=0, default_loss_r=1, excluded_index=None):
+    
+    for epoch in range(num_epochs):
+        
+        if epoch == 0:
+            
+            # Generate subset data loader based on dataset
+            if dataset == 'cifar10':
+                train_loader, val_loader, test_loader = get_cifar10_loaders_sub(subset_rate, excluded_index)
+            elif dataset == 'cifar100':
+                train_loader, val_loader, test_loader = get_cifar100_loaders()
+            elif dataset == 'svhn':
+                train_loader, val_loader, test_loader = get_svhn_loaders()
+            
+            # Record accuaracy after every epoch
+            water_test_acc = []
+            train_test_acc = []
+            water_query_acc = []
+            train_query_acc = []
+            neuron_loss_after_epoch = [] # To reduce variable, first item stores the acc of water model on testing before training 
+            task_loss_after_epoch = [] # To reduce variable, first item stores the acc of water model on query before training
+            
+            # Record the input of every hooked layer
+            water_relu = []
+            train_relu = []
+            
+            # Define the hook function
+            w_hooks = [] # list of hook handles, to be removed when you are done
+            t_hooks = []
+            hook_flag = False
+            def water_hook(module, input, output):
+                if hook_flag:
+                    nonlocal water_relu
+                    water_relu.append(output)   
+            def train_hook(module, input, output):
+                if hook_flag:
+                    nonlocal train_relu
+                    train_relu.append(output)
+
+            # Hook the function onto conv1 and conv2 of layer1~layer4 of both models.
+            for idx in range(len(water_model[1].layer1)):
+                for name, module in water_model[1].layer1[idx].named_children():
+                    if name in ['bn1']:
+                        w_hooks.append(getattr(water_model[1].layer1[idx], name).register_forward_hook(water_hook))
+            """" for idx in range(len(water_model[1].layer2)):
+                for name, module in water_model[1].layer2[idx].named_children():
+                    if name in ['conv1','conv2']:
+                        w_hooks.append(getattr(water_model[1].layer2[idx], name).register_forward_hook(water_hook)) """
+            """ for idx in range(len(water_model[1].layer3)):
+                for name, module in water_model[1].layer3[idx].named_children():
+                    if name in ['conv1','conv2']:
+                        w_hooks.append(getattr(water_model[1].layer3[idx], name).register_forward_hook(water_hook)) """
+            """ for idx in range(len(water_model[1].layer4)):
+                for name, module in water_model[1].layer4[idx].named_children():
+                    if name in ['conv1','conv2']:
+                        w_hooks.append(getattr(water_model[1].layer4[idx], name).register_forward_hook(water_hook)) """
+           
+
+            for idx in range(len(train_model[1].layer1)):
+                for name, module in train_model[1].layer1[idx].named_children():
+                    if name in ['bn1']:
+                        t_hooks.append(getattr(train_model[1].layer1[idx], name).register_forward_hook(train_hook))
+            """ for idx in range(len(train_model[1].layer2)):
+                for name, module in train_model[1].layer2[idx].named_children():
+                    if name in ['conv1','conv2']:
+                        t_hooks.append(getattr(train_model[1].layer2[idx], name).register_forward_hook(train_hook)) """
+            """ for idx in range(len(train_model[1].layer3)):
+                for name, module in train_model[1].layer3[idx].named_children():
+                    if name in ['conv1','conv2']:
+                        t_hooks.append(getattr(train_model[1].layer3[idx], name).register_forward_hook(train_hook)) """
+            """ for idx in range(len(train_model[1].layer4)):
+                for name, module in train_model[1].layer4[idx].named_children():
+                    if name in ['conv1','conv2']:
+                        t_hooks.append(getattr(train_model[1].layer4[idx], name).register_forward_hook(train_hook)) """
+           
+            
+            print("Watered model Test Acc before training:")
+            neuron_loss_after_epoch.append(model_on_testset(water_model, test_loader, device))
+            print("Watered model Query Acc before training:")
+            task_loss_after_epoch.append(round(model_on_queryset(water_model, query, response, device).item(), 4))         
+            
+        print(f"Now in epoch {epoch+1}...")
+
+        ave_neu_loss_per_epoch = 0.0
+        ave_task_loss_per_epoch = 0.0
+        
+        for batch_idx, batch in enumerate(train_loader):
+            
+            hook_flag = True
+            # Record the input of every hooked layer
+            water_relu = []
+            train_relu = []
+            
+            optimizer.zero_grad()
+            images = batch[0]
+            labels = batch[1].long()
+                        
+            images, labels = images.to(device), labels.to(device)
+            
+            train_model.train()
+            water_model.eval() # If not set to eval(), the model will still changing even if not being opt.step()
+            
+            outputs = train_model(images)
+            with torch.no_grad():
+                outputs_water = water_model(images) 
+            
+            if not water_relu and not train_relu:
+                print("No value stored in fix_relu and train_relu...")
+                breakpoint()
+            elif len(water_relu) != len(train_relu):
+                print("The length of water_relu and train_relu are not equal...")
+                breakpoint()
+            
+            if batch_idx % 5 == 0:
+                print(f"{len(water_relu)} layers are store in the water_lists...")
+                print(f"{len(train_relu)} layers are store in the train_lists...")    
+            
+             # Reset new_loss
+            new_loss = 0.0
+            # Sum up the loss of conv1 and conv2 of layer1~layer4 of both models
+            for idx in range(len(water_relu)):
+                new_loss += custom_loss1(water_relu[idx][0].detach(), train_relu[idx][0]) / len(water_relu)
+                #print(">>>>>>>>>>>>>>>>>>>>>new loss:",new_loss.item())
+                #breakpoint()
+            # Just for debug
+            if batch_idx % 5 == 0:
+                print(f"{batch_idx+1} batch neuron loss: {new_loss.item()}")    
+            
+            
+            kd_loss = loss_fn_kd(outputs, labels, outputs_water, 0.9, 20)
+            if batch_idx % 5 == 0:
+                print(f"{batch_idx+1} batch kd loss: {kd_loss.item()}")      
+            
+            # Combine both losses
+            loss = (default_loss_r)*kd_loss + (new_loss_r)*(new_loss)
+            if batch_idx % 5 == 0:
+                print(f"{batch_idx+1} batch combined loss: {loss.item()}")
+            
+            ave_neu_loss_per_epoch += new_loss.item()
+            ave_task_loss_per_epoch += kd_loss.item()
+            
+            
+            #train_model.train()
+            loss.backward()
+            optimizer.step()
+        
+        """ neuron_loss_after_epoch.append(round(new_loss.item(),4))
+        crosse_loss_after_epoch.append(round(ce_loss.item(),4)) """
+        ave_task_loss_per_epoch /= len(train_loader)
+        ave_neu_loss_per_epoch /= len(train_loader)
+        neuron_loss_after_epoch.append(round(ave_neu_loss_per_epoch,4))
+        task_loss_after_epoch.append(round(ave_task_loss_per_epoch,4))
+        
+        print("===============================1 epoch of training ends===============================")
+        
+        # Validate the model
+        train_model.eval()
+        hook_flag = False # Turn off the hook for validation
+        val_loss = 0.0
+        correct = 0
+        total = 0
+        print("Validation Process...")
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(val_loader):
+                images = batch[0]
+                labels = batch[1].long()
+                images, labels = images.to(device), labels.to(device)
+                outputs = train_model(images)
+                loss = F.cross_entropy(outputs, labels)
+                val_loss += loss.item() * images.size(0)
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+            val_epoch_loss = val_loss / len(val_loader.dataset)
+            val_accuracy = correct / total
+            print(f"Epoch {epoch+1}/{num_epochs}, Validation Loss: {val_epoch_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}")
+          
+        #Testing both model
+        print("Train Model Test Process...")
+        train_test_acc.append(model_on_testset(train_model, test_loader, device))
+        print("Water Model Test Process...")
+        water_test_acc.append(model_on_testset(water_model, test_loader, device))
+
+        # Query both models
+        print("Train Model Query Process...")
+        train_query_acc.append(round(model_on_queryset(train_model, query, response, device).item(),4))
+        print("Water Model Query Process...")
+        water_query_acc.append(round(model_on_queryset(water_model, query, response, device).item(),4))
+        
+    # Remove hooks for model after used and reset the handle lists
+    for handle in w_hooks:
+        handle.remove()
+    for handle in t_hooks:
+        handle.remove()
+    w_hooks=[]
+    t_hooks=[]
+    
+    print('===============================Finished Training===============================')
+    print('===============================Finished Training===============================')
+    print('===============================Finished Training===============================')
+    print(f"Neuron loss after every epoch: {neuron_loss_after_epoch}")
+    print(f"K.D. loss after every epoch: {task_loss_after_epoch}")    
+    return train_test_acc, train_query_acc, water_test_acc, water_query_acc       
+
 def model_on_testset(test_model, test_loader, device):
     test_model.eval()
     correct = 0
@@ -772,7 +1007,7 @@ if __name__ == "__main__":
     ########################### Hyperparameters setting ###########################
     dataset = 'cifar10'
     subset_rate = 0.1 # 0~1
-    epoch = 1
+    epoch = 10
     default_loss_ratio = 1 # >=0
     new_loss_ratio = 1# >=0
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -814,7 +1049,7 @@ if __name__ == "__main__":
     water_model.to(device)
 
     # Start training
-    train_test_acc, train_query_acc, water_test_acc, water_query_acc = start_train_kd_r(dataset, subset_rate, train_model, water_model, opt, device, query, response, epoch, new_loss_ratio, default_loss_ratio, query_indices)
+    train_test_acc, train_query_acc, water_test_acc, water_query_acc = start_train_kd_loss1(dataset, subset_rate, train_model, water_model, opt, device, query, response, epoch, new_loss_ratio, default_loss_ratio, query_indices)
     
     # Print the results
     print(f"===============================Training on {subset_rate} of {dataset} with old/new loss ratio {default_loss_ratio}/{new_loss_ratio} for {epoch} epochs.===============================")
@@ -824,6 +1059,8 @@ if __name__ == "__main__":
     print("Water model Query Acc:", water_query_acc)
     
     breakpoint()
+    
+
     
 
     
